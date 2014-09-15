58946b13c796629181d40c441b2e5c331338e725
Andrew Olson <olson@cshl.edu>
2014-09-15 13:28:30 -0400

    testing BSON Binary mongodb roundtrip

--- app.js
+++ app.js
@@ -1,49 +1,42 @@
-// var Db = require('mongodb').Db,
-//     MongoClient = require('mongodb').MongoClient,
-//     Server = require('mongodb').Server,
-//     ReplSetServers = require('mongodb').ReplSetServers,
-//     ObjectID = require('mongodb').ObjectID,
-//     Binary = require('mongodb').Binary,
-//     GridStore = require('mongodb').GridStore,
-//     Grid = require('mongodb').Grid,
-//     Code = require('mongodb').Code,
-//     BSON = require('mongodb').pure().BSON,
-// var     assert = require('assert');
-// var MongoClient = require('mongodb').MongoClient;
-var CmpBitVec = require('./CmpBitVec');
-var v1 = new CmpBitVec();
-v1.appendFill1(100);
-v1.appendFill0(100);
-console.log("v1",v1);
-var v2 = new CmpBitVec();
-v2.appendFill0(50);
-v2.appendFill1(150);
-v2.appendFill0(50);
-console.log("v2", v2);
-var v3 = v1.or(v2);
-v3.unpack();
-console.log("v1.or(v2)",v3);
-var v4 = new CmpBitVec();
-v4.appendFill1(200);
-v4.appendFill0(50);
-console.log("correct or",v4);
-v3 = v1.and(v2);
-v3.unpack();
-console.log("v1.and(v2)",v3);
-var v5 = new CmpBitVec();
-v5.appendFill0(50);
-v5.appendFill1(50);
-v5.appendFill0(150);
-console.log("correct and",v5);
+var MongoClient = require('mongodb').MongoClient
+  , BSON = require('mongodb').BSONPure
+  , assert = require('assert')
+  , CmpBitVec = require('./CmpBitVec');
 
-  // // Connect using the connection string
-  // MongoClient.connect("mongodb://localhost:27017/test", {native_parser:true}, function(err, db) {
-  //   assert.equal(null, err);
-  // 
-  //   db.collection('bvec').update({a:1}, {b:1}, {upsert:true}, function(err, result) {
-  //     assert.equal(null, err);
-  //     assert.equal(1, result);
-  // 
-  //     db.close();
-  //   });
-  // });
+var v = new CmpBitVec();
+v.appendFill0(10);
+v.appendFill1(100);
+v.appendFill0(10);
+console.log(v.toString());
+var buf = v.saveToArrayBuffer();
+// console.log(buf);
+var bsonbin = new BSON.Binary(new Buffer(new Uint8Array(buf)));
+// console.log("bsonbin",bsonbin);
+
+function toArrayBuffer(buffer) {
+    var ab = new ArrayBuffer(buffer.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return ab;
+}
+
+// Connect using the connection string
+MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+  assert.equal(null, err);
+  db.collection('bvec', function(err,collection) {
+    collection.remove(function(err, result) {
+      collection.insert({'binary':bsonbin}, function(err, doc) {
+        collection.findOne(function(err, document) {
+          var v2 = new CmpBitVec();
+          v2.loadFromArrayBuffer(toArrayBuffer(document.binary.buffer));
+          console.log(v2.toString());
+          collection.remove(function(err, collection) {
+            db.close();
+          });
+        });
+      });
+    });
+  });
+});
